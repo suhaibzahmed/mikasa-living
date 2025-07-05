72d63e02cf28ad382695325ad27197c81c93e934
suhaibzahmed <suhaib.z.ahmed@gmail.com>
2025-07-05 14:12:09 +0530

    removed registration stepper

--- app/(auth)/vendor/sign-up/page.tsx
+++ app/(auth)/vendor/sign-up/page.tsx
@@ -1,32 +1,8 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { VendorRegistrationProvider } from '@/lib/VendorRegistrationContext'
-import VendorRegistrationStepper from './_components/VendorRegistrationStepper'
-
 const VendorRegisterPage = () => {
   return (
-    <VendorRegistrationProvider>
-      <div className="flex flex-col gap-y-4 min-h-svh w-full items-center justify-center ">
-        <h4>Vendor Registration</h4>
-
-        <Card className="min-w-md">
-          <CardHeader>
-            <CardTitle>Join Our Marketplace</CardTitle>
-            <CardDescription>
-              Please enter your details to join our marketplace
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <VendorRegistrationStepper />
-          </CardContent>
-        </Card>
-      </div>
-    </VendorRegistrationProvider>
+    <div className="flex flex-col gap-y-4 min-h-svh w-full items-center justify-center ">
+      <h4>Vendor Registration</h4>
+    </div>
   )
 }
 
